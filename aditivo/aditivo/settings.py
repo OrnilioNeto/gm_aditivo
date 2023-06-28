72f907a7387774bb2db5ec2920037edb7e9c989e
Ornilio Neto <orniliooficial@gmail.com>
2023-06-28 15:52:25 -0300

    arquivos staticos

--- aditivo/aditivo/settings.py
+++ aditivo/aditivo/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'aditivo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'railway',
+        'USER':'postgres',
+        'PASSWORD':'qT5vt7sBU38O275dMDEI',
+        'HOST':'containers-us-west-201.railway.app',
+        'PORT':'7292',
     }
 }
 
@@ -128,6 +139,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
