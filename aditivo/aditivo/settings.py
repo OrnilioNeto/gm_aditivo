faa83ac4fdda06043f07010f01d18aa87a4f5efb
Ornilio Neto <orniliooficial@gmail.com>
2023-06-29 15:45:03 -0300

    correcao stattic

--- aditivo/aditivo/settings.py
+++ aditivo/aditivo/settings.py
@@ -137,7 +137,7 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
